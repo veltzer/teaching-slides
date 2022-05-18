c8a8241f4c05f420dd9edf0da209438e3cc89c29
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-18 13:51:13 +0300

    new config

--- config/project.py
+++ config/project.py
@@ -1,4 +1,1 @@
-project_name = "teaching-slides"
-project_long_description = "Slides for my teaching"
-project_year_started = 2013
-project_description = "Slides for my teaching"
+description_short = "Slides for my teaching"
