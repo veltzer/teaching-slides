981730115265a0b5733dd7eee06d57281e44eb47
Mark Veltzer <mark.veltzer@gmail.com>
2025-02-08 19:38:29 +0200

    linting py code

--- scripts/wrapper_pdflatex.py
+++ scripts/wrapper_pdflatex.py
@@ -1,10 +1,5 @@
 #!/usr/bin/env python
 
-import sys
-import os
-import os.path
-import subprocess
-
 """
 This is a script that runs pdflatex for us.
 Why do we need this script ?
@@ -26,16 +21,21 @@ mode to ask the user what to do about an error (what is this behaviour anyway ?!
 This python script is a rewrite of a similar script in perl.
 """
 
+import sys
+import os
+import os.path
+import subprocess
+
 #parameters
 # do you want debugging...
-debug = False
+DEBUG = False
 # remove the tmp file for output at the end of the run? (this should be yes
 # unless you want junk files hanging around in /tmp...)
-remove_tmp = True
+REMOVE_TMP = True
 # how many times to run 'pdflatex(1)' ?
-runs = 2
+RUNS = 2
 # do you want to run the 'qpdf' post processing stage?
-qpdf = True
+QPDF = True
 
 
 def printout(filename: str):
@@ -45,9 +45,9 @@ def printout(filename: str):
     only prints the lines between lines starting with '!' (including the actual lines
     starting with '!'). Apparently this is how pdflatex shows errors. Ugrrr...
     """
-    if debug:
+    if DEBUG:
         print(f"printing [{filename}]", file=sys.stderr)
-    with open(filename):
+    with open(filename, encoding="UTF8") as file:
         inerr = False
         for line in file:
             if inerr:
@@ -64,14 +64,15 @@ def unlink_check(filename: str, check: bool, doit: bool):
     this is a function that removes a file and can optionally die if there is a problem
     """
     if doit:
-        if debug:
+        if DEBUG:
             print(f"unlinking [{filename}]", file=sys.stderr)
         if check:
             os.unlink(filename)
         else:
             try:
                 os.unlink(filename)
-            except:
+            # pylint: disable=broad-exception-caught
+            except Exception as _:
                 pass
 
 
@@ -79,26 +80,28 @@ def chmod_check(filename:str, check: bool):
     """
     this is a function that chmods a file and can optionally die if there is a problem
     """
-    if debug:
+    if DEBUG:
         print(f"chmodding [{filename}]", file=sys.stderr)
     if check:
         os.chmod(filename, 0o444)
     else:
         try:
             os.chmod(filename, 0o444)
-        except:
+        # pylint: disable=broad-exception-caught
+        except Exception as _:
             pass
 
 
 def my_call(args):
-    if debug:
+    """ run subprocess """
+    if DEBUG:
         print(f"my_call args are [{args}]", file=sys.stderr)
     res = subprocess.check_call(
         args,
         stdout=subprocess.DEVNULL,
         stderr=subprocess.DEVNULL,
     )
-    if debug:
+    if DEBUG:
         print(f"my_call res is [{res}]", file=sys.stderr)
     return res
 
@@ -107,63 +110,70 @@ def my_rename(old_filename: str, new_filename: str, check: bool):
     """
     this is a function that renames a file and dies if there is a problem
     """
-    if debug:
+    if DEBUG:
         print(f"my_rename [{old_filename, new_filename}]", file=sys.stderr)
     if check:
         os.rename(old_filename, new_filename)
     else:
         try:
             os.rename(old_filename, new_filename)
-        except:
+        # pylint: disable=broad-exception-caught
+        except Exception as _:
             pass
 
-# here we go...
-filename_input = sys.argv[1]
-filename_output = sys.argv[2]
-output_dir = os.path.dirname(filename_output)
-output_base = os.path.splitext(filename_output)[0]
+def main():
+    """ main entry point """
+    filename_input = sys.argv[1]
+    filename_output = sys.argv[2]
+    output_dir = os.path.dirname(filename_output)
+    output_base = os.path.splitext(filename_output)[0]
 
-args = [
-    "pdflatex",
-    "-shell-escape",
-    "-interaction=nonstopmode",
-    "-halt-on-error",
-    "-output-directory",
-    output_dir,
-    filename_input,
-]
-if debug:
-    print(f"input is [{filename_input}]")
-    print(f"output is [{filename_output}")
-    print(f"cmd is [{args}")
-# first remove the output (if it exists)
-unlink_check(
-    filename_output,
-    True,
-    os.path.isfile(filename_output),
-)
-# we need to run the command twice!!! (to generate the index and more)
-for _ in range(runs):
-    my_call(args)
-    unlink_check(output_base+'.log', True, True)
-    unlink_check(output_base+'.out', True, True)
-    unlink_check(output_base+'.toc', True, True)
-    unlink_check(output_base+'.aux', True, True)
-    unlink_check(output_base+'.nav', True, True)
-    unlink_check(output_base+'.snm', True, True)
-    unlink_check(output_base+'.vrb', True, True)
-
-if qpdf:
-    # move the output to the new place
-    tmp_output = filename_output+'.tmp'
-    my_rename(filename_output, tmp_output, True)
-    # I also had '--force-version=1.5' but it is not needed since I use pdflatex and pdftex with the right version there...
     args = [
-        "qpdf",
-        "--deterministic-id",
-        "--linearize",
-        tmp_output,
-        filename_output,
+        "pdflatex",
+        "-shell-escape",
+        "-interaction=nonstopmode",
+        "-halt-on-error",
+        "-output-directory",
+        output_dir,
+        filename_input,
     ]
-    my_call(args)
-    unlink_check(tmp_output, True, True)
+    if DEBUG:
+        print(f"input is [{filename_input}]")
+        print(f"output is [{filename_output}")
+        print(f"cmd is [{args}")
+    # first remove the output (if it exists)
+    unlink_check(
+        filename_output,
+        True,
+        os.path.isfile(filename_output),
+    )
+    # we need to run the command twice!!! (to generate the index and more)
+    for _ in range(RUNS):
+        my_call(args)
+        unlink_check(output_base+'.log', True, True)
+        unlink_check(output_base+'.out', True, True)
+        unlink_check(output_base+'.toc', True, True)
+        unlink_check(output_base+'.aux', True, True)
+        unlink_check(output_base+'.nav', True, True)
+        unlink_check(output_base+'.snm', True, True)
+        unlink_check(output_base+'.vrb', True, True)
+
+    if QPDF:
+        # move the output to the new place
+        tmp_output = filename_output+'.tmp'
+        my_rename(filename_output, tmp_output, True)
+        # I also had '--force-version=1.5' but it is not needed since I use
+        # pdflatex and pdftex with the right version there...
+        args = [
+            "qpdf",
+            "--deterministic-id",
+            "--linearize",
+            tmp_output,
+            filename_output,
+        ]
+        my_call(args)
+        unlink_check(tmp_output, True, True)
+
+
+if __name__ == "__main__":
+    main()
